8317b54b3609138a1257b07c6afa01dd8bf57ccd
Vertex-3D <70529532+Vertex-3D@users.noreply.github.com>
2021-05-19 20:50:02 +0530

    Add files via upload

---
+++ AnimationController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    int movex = Animator.StringToHash("X_move");
+    int movey = Animator.StringToHash("Y_move");
+    int isJumping = Animator.StringToHash("isJumping");
+    int isGrounded = Animator.StringToHash("isGrounded");
+
+    Animator anim;
+    private void Awake()
+    {
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    public void JumpingAnimation(bool value)
+    {
+        if (anim.GetBool(isJumping) != value)
+            anim.SetBool(isJumping, value);
+    }
+
+    public void isGruondedFunc(bool value)
+    {
+        if (anim.GetBool(isGrounded) != value)
+            anim.SetBool(isGrounded, value);
+    }
+
+    public void playerMovementAnimation(Vector2 value)
+    {
+        anim.SetFloat(movex, value.x);
+        anim.SetFloat(movey, value.y);
+    }
+}
